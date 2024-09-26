ecb681e41a6434522a56bcfd8a6df823e2e5e1d0
Murtaza Sediqi <148034829+sediqi09@users.noreply.github.com>
2024-09-26 21:25:48 +0500

    Add files via upload

---
+++ model_of_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int abs(int x) {
+    if (x < 0) {
+        return -x;
+    }
+    return x;
+}
+
+int main() {
+    std::cout << "abs(5) = " << abs(5) << std::endl;
+    std::cout << "abs(-3) = " << abs(-3) << std::endl;
+    std::cout << "abs(0) = " << abs(7) << std::endl;
+    std::cout << "abs(-100) = " << abs(-100) << std::endl;
+
+    return 0;
+}
